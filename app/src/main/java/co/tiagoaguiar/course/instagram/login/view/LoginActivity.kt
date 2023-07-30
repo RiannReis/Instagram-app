4746aac6791f821322ab2deea7f94baada9cbdc0
Riann Silva <ooriannsilva@gmail.com>
2023-07-30 15:51:17 -0300

    TextWatcher: Changing Form with Inputs

--- app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -2,7 +2,11 @@ package co.tiagoaguiar.course.instagram.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import co.tiagoaguiar.course.instagram.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
@@ -10,7 +14,35 @@ class LoginActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
 
-    findViewById<TextInputLayout>(R.id.login_edit_email_input)
-      .error = "e-mail inválido!"
+    val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+    val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+    editTextEmail.addTextChangedListener(watcher)
+    editTextPassword.addTextChangedListener(watcher)
+
+    findViewById<Button>(R.id.login_btn_enter).setOnClickListener {
+
+      findViewById<TextInputLayout>(R.id.login_edit_email_input)
+        .error = "e-mail inválido!"
+
+      findViewById<TextInputLayout>(R.id.login_edit_password_input)
+        .error = "senha incorreta!"
+    }
+
+  }
+
+  private val watcher = object: TextWatcher{
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+      findViewById<Button>(R.id.login_btn_enter).isEnabled = s.toString().isNotEmpty()
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+
+    }
+
   }
 }
