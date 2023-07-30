858c1e3db2b30be3a07021066f42a1e2413da358
Riann Silva <ooriannsilva@gmail.com>
2023-07-30 15:32:28 -0300

    Form Validation

--- app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/login/view/LoginActivity.kt
@@ -3,10 +3,14 @@ package co.tiagoaguiar.course.instagram.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.tiagoaguiar.course.instagram.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
+
+    findViewById<TextInputLayout>(R.id.login_edit_email_input)
+      .error = "e-mail inválido!"
   }
 }
