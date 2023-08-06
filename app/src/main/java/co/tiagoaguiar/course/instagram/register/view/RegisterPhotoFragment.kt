43800342d09cc2338fdda22ed101f1989a80a0c2
Riann Silva <ooriannsilva@gmail.com>
2023-08-06 13:13:19 -0300

    Custom Dialog - UI Components Programmatically

--- app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,11 +1,13 @@
 package co.tiagoaguiar.course.instagram.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
 
@@ -15,5 +17,23 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when (it.id) {
+                R.string.photo -> {
+                    Log.i("teste", "foto")
+                }
+                R.string.gallery -> {
+                    Log.i("teste", "galeria")
+                }
+            }
+        }
+        customDialog.show()
     }
 }
