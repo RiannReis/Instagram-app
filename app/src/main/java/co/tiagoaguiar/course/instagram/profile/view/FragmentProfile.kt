9064e50f5548c00c06a348de7ded3c9549f98644
Riann Silva <ooriannsilva@gmail.com>
2023-08-06 23:16:28 -0300

    User Profile Screen

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/profile/view/FragmentProfile.kt
@@ -0,0 +1,20 @@
+package co.tiagoaguiar.course.instagram.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.tiagoaguiar.course.instagram.R
+
+class FragmentProfile : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
