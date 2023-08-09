dcdbf9f2ce6d4efb1356f4bb931a6687ca5f17c6
Riann Silva <ooriannsilva@gmail.com>
2023-08-09 17:40:08 -0300

    Status Bar Programmatically

--- app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
@@ -1,12 +1,22 @@
 package co.tiagoaguiar.course.instagram.main.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import co.tiagoaguiar.course.instagram.R
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
     }
 }
