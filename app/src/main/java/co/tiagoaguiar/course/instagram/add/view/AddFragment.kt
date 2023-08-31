8a0e260a8e5a761a8f74c83efb509e1c43bb0c1e
Riann Silva <ooriannsilva@gmail.com>
2023-08-31 17:38:34 -0300

    Camera Preview

--- app/src/main/java/co/tiagoaguiar/course/instagram/add/view/AddFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/add/view/AddFragment.kt
@@ -2,14 +2,16 @@ package co.tiagoaguiar.course.instagram.add.view
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import co.tiagoaguiar.course.instagram.R
 import co.tiagoaguiar.course.instagram.add.Add
 import co.tiagoaguiar.course.instagram.common.base.BaseFragment
 import co.tiagoaguiar.course.instagram.databinding.FragmentAddBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
@@ -21,7 +23,7 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
         const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 
-    override lateinit  var presenter: Add.Presenter
+    override lateinit var presenter: Add.Presenter
 
     override fun setupViews() {
         val tabLayout = binding?.addTab
@@ -30,6 +32,20 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
         viewPager?.adapter = adapter
 
         if (tabLayout != null && viewPager != null) {
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    if (tab?.text == getString(adapter.tabs[0]))
+                        startCamera()
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    
+                }
+            })
             TabLayoutMediator(tabLayout, viewPager) { tab, position ->
                 tab.text = getString(adapter.tabs[position])
             }.attach()
@@ -48,19 +64,26 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
     }
 
     private fun startCamera() {
-        //TODO: iniciar a camera
-        Log.i("teste", "startCamera")
+        setFragmentResult("cameraKey", bundleOf("startCamera" to true))
     }
 
     private fun allPermissionsGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
-    private val getPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            Toast.makeText(requireContext(), R.string.permission_camera_denied, Toast.LENGTH_LONG).show()
+    private val getPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    R.string.permission_camera_denied,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
-    }
 }
--- app/src/main/java/co/tiagoaguiar/course/instagram/add/view/CameraFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/add/view/CameraFragment.kt
@@ -1,11 +1,23 @@
 package co.tiagoaguiar.course.instagram.add.view
 
 import android.os.Bundle
-import android.view.*
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import co.tiagoaguiar.course.instagram.R
+import java.lang.Exception
 
-class CameraFragment : Fragment(){
+class CameraFragment : Fragment() {
+
+    private lateinit var previewView: PreviewView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -13,6 +25,49 @@ class CameraFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        previewView = view.findViewById(R.id.camera_img)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener("cameraKey") { requestKey, bundle ->
+            val shouldStart = bundle.getBoolean("startCamera")
+
+            if (shouldStart) {
+                startCamera()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+
+            } catch (e: Exception) {
+                Log.e("teste", "Failure to initialize the camera", e)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
 }
